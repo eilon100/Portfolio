151fb6dcfe9e80b2010c3d07e7c34027253c19f0
eilon <eilonshamir123@gmail.com>
2023-03-03 18:59:42 +0200

    fix responsive

--- components/About.tsx
+++ components/About.tsx
@@ -4,16 +4,16 @@ import img from "../public/profileImage.jpeg";
 const About: FC = () => {
   return (
     <motion.div
-      className="flex flex-col relative h-screen mt-24 text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+      className="flex flex-col gap-10 md:justify-evenly md:gap-0 pt-[20vh] md:pt-0 relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 mx-auto items-center"
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
     >
-      <h3 className="absolute top-24 uppercase tracking-[20px] ml-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-[12%] uppercase tracking-[20px] ml-[20px] text-gray-500 text-2xl">
         About
       </h3>
       <motion.img
-        className="-mb-28 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]"
+        className="md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]"
         src={img.src}
         initial={{ y: 50, opacity: 0 }}
         transition={{ duration: 1.2 }}
--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -5,12 +5,12 @@ import ExperienceCard from "./ExperienceCard";
 const WorkExperience: FC = () => {
   return (
     <motion.div
-      className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
+      className="h-screen flex relative gap-10 md:justify-evenly md:gap-0 pt-[20vh] md:pt-0 overflow-hidden flex-col text-left md:flex-row max-w-full px-10 mx-auto items-center"
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
     >
-      <h3 className="absolute top-24 uppercase tracking-[20px] ml-[20px] text-gray-500 text-2xl">
+      <h3 className=" absolute top-[12%] uppercase tracking-[20px] ml-[20px] text-gray-500 text-2xl">
         Experience
       </h3>
       <div className="w-full flex justify-center">
