722e9b213604242a757ed3ee85c5ed2370a0921a
eilon <eilonshamir123@gmail.com>
2023-03-03 17:28:11 +0200

    fix errors

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -20,7 +20,9 @@ const Skill: FC<IProps> = ({ directionLeft, name }) => {
       />
       <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-16 h-16 sm:w-24 sm:h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
         <div className="flex items-center justify-center h-full ">
-          <p className="text-xs sm:text-lg md:text-xl font-semibold capitalize text-black opacity-100">{name}</p>
+          <p className="text-xs sm:text-lg md:text-xl font-semibold capitalize text-black opacity-100">
+            {name}
+          </p>
         </div>
       </div>
     </div>
